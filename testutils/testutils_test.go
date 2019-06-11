6a65dc6982f05c5f309f06e0e871be626f8318a2
Alex <alext@rtradetechnologies.com>
2019-06-10 19:22:38 -0700

    testutils: add libp2p test

--- testutils/testutils_test.go
+++ testutils/testutils_test.go
@@ -2,6 +2,9 @@ package testutils
 
 import (
 	"testing"
+	"sync"
+	"context"
+	"github.com/multiformats/go-multiaddr"
 )
 
 func Test_NewPrivateKey(t *testing.T) {
@@ -38,4 +41,20 @@ func Test_NewKeystore(t *testing.T) {
 	if keystore := NewKeystore(t); keystore == nil {
 		t.Fatal("should not be nil")
 	}
+}
+
+
+func Test_SetupLibp2p(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	wg := &sync.WaitGroup{}
+	logger := NewLogger(t)
+	pk := NewPrivateKey(t)
+	pstore := NewPeerstore(t)
+	dstore := NewDatastore(t)
+	addrs := []multiaddr.Multiaddr{NewMultiaddr(t)}
+	host, dht := SetupLibp2p(ctx, wg, logger.Desugar(), pk, nil, addrs, pstore, dstore, t)
+	host.Close()
+	dht.Close()
+	cancel()
+	wg.Wait()
 }
