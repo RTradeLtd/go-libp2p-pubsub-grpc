fbb272681a435bd70ea9034a758eea3df73dd4dd
Alex <alext@rtradetechnologies.com>
2019-06-10 20:43:14 -0700

    server: add better tests

--- server_test.go
+++ server_test.go
@@ -2,11 +2,16 @@ package libpubsubgrpc
 
 import (
 	"context"
+	"fmt"
 	"testing"
 	"time"
 
+	//"time"
+
+	"io"
 	"sync"
 
+	pb "github.com/RTradeLtd/go-libp2p-pubsub-grpc/pb"
 	tutil "github.com/RTradeLtd/go-libp2p-pubsub-grpc/testutils"
 	ps "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/multiformats/go-multiaddr"
@@ -44,7 +49,85 @@ func Test_Server(t *testing.T) {
 			t.Fatal(err)
 		}
 	}()
+
+	client, err := NewClient("", "", "127.0.0.1:9090")
+	if err != nil {
+		cancel()
+		t.Fatal(err)
+	}
+
+	subStream, err := client.Subscribe(ctx, &pb.SubscribeRequest{Topic: "hello"})
+	if err != nil {
+		cancel()
+		t.Fatal(err)
+	}
+	defer subStream.CloseSend()
+	go func() {
+		for {
+			msg, err := subStream.Recv()
+			if err != nil && err == io.EOF {
+				return
+			}
+
+			if msg.GetFrom() != nil {
+				fmt.Println("from", string(msg.GetFrom()))
+			}
+			if msg.GetData() != nil {
+				fmt.Println("data", string(msg.GetData()))
+			}
+			if msg.GetSeqno() != nil {
+				fmt.Println("seqno", string(msg.GetSeqno()))
+			}
+			if msg.GetTopicIDs() != nil {
+				fmt.Println("topicIDs", msg.GetTopicIDs())
+			}
+			if msg.GetSignature() != nil {
+				fmt.Println("signature", msg.GetSignature())
+			}
+			if msg.GetKey() != nil {
+				fmt.Println("key", msg.GetKey())
+			}
+		}
+	}()
+	// test publishing
+	pubStream, err := client.Publish(ctx)
+	if err != nil {
+		cancel()
+		t.Fatal(err)
+	}
+	if err := pubStream.Send(&pb.PublishRequest{Topic: "hello", Data: []byte("world")}); err != nil {
+		cancel()
+		t.Fatal(err)
+	}
+	if _, err := pubStream.CloseAndRecv(); err != nil && err != io.EOF {
+		cancel()
+		t.Fatal(err)
+	}
+	if _, err := client.ListPeers(ctx, &pb.ListPeersRequest{Topics: []string{"hello"}}); err != nil {
+		cancel()
+		t.Fatal(err)
+	}
+	var foundHelloTopic bool
+	resp, err := client.GetTopics(ctx, &pb.Empty{})
+	if err != nil {
+		cancel()
+		t.Fatal(err)
+	}
+	for _, topic := range resp.GetNames() {
+		if topic == "hello" {
+			foundHelloTopic = true
+			break
+		}
+	}
+	if !foundHelloTopic {
+		cancel()
+		t.Fatal("failed to find hello topic")
+	}
+	// we need to sleep to give enough time for our goroutine
+	// that processes messages to have enough time to pick up the sent messages
 	time.Sleep(time.Second * 10)
+	// end publishing test
+	client.Close()
 	cancel()
 	wg.Wait()
 }
